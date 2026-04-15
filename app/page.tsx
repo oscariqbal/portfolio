8da696b780c466f7f359e86b46e4b268712f08b5
Oscar Iqbal Mustofa <oscariqbal75@gmail.com>
2026-04-15 19:50:53 +0700

    project section inisialization

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,64 @@
-import Link from 'next/link';
+import Image from "next/image";
+import { Card, CardTitle, CardDescription, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div>
-      <header className="h-[6vh] w-full bg-background sticky top-0 z-50 flex items-center border border-blue-500">
-        <nav className='border border-red-500 font-heading container mx-auto'>
-          <ul className='flex items-center gap-6'>
-            <li><a href='#top'>Top</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contact'>Contact</a></li>
+      <header className="h-[10vh] w-full sticky top-0 z-50 flex items-end">
+        <nav className='h-[70%] font-heading container mx-auto bg-foreground/10 backdrop-blur-sm rounded-md pl-5 border border-foreground/15'>
+          <ul className='h-full w-full flex items-center gap-6'>
+            <li>
+              <a href='#top'>
+                <Image src="/logo.svg" alt="icon" width={30} height={30} className="rounded-sm"/>
+              </a>
+            </li>
+            <li><a className="hover:text-muted-foreground" href='#projects'>Projects</a></li>
+            <li><a className="hover:text-muted-foreground" href='#contact'>Contact</a></li>
           </ul>
         </nav>
       </header>
       <main>
-        <section id='top' className='h-[60vh] w-full bg-linear-to-b from-background from-95% to-chart-2 to-100%'>
-          <div className='h-full w-full container mx-auto border border-red-500'>
-            
+        <section id='top' className='h-[60vh] w-full bg-linear-to-b from-background from-95% to-chart-4 to-100%'>
+          <div className='h-full w-full container mx-auto flex justify-between'>
+            <div className="h-full w-[50%] flex flex-col gap-5 justify-center">
+              <h1 className="font-heading text-4xl">Oscar Iqbal Mustofa</h1>
+              <p className="text-justify opacity-60">
+                I am an Informatics graduate specializing in Data Science, Artificial Intelligence, and Web Development. Proficient in several programming languages, including Python, JavaScript, and PHP.
+              </p>
+            </div>
+            {/*
+            <div className="h-full w-[47.5%]">
+              <div className="h-full w-full flex items-center justify-center">
+                <Image
+                    className=""
+                    src="/img/fireworks.png"
+                    alt="fireworks.png"
+                    width={250}
+                    height={250}
+                />
+              </div>
+            </div>
+            */}
           </div>
         </section>
-        <section id='projects' className='h-[164vh] w-full bg-linear-to-b from-chart-2 from-10% to-foreground to-100%'>
-          <div className='h-full w-full container mx-auto border border-red-500'>
-
+        <section id='projects' className='h-[164vh] w-full bg-linear-to-b from-chart-4 from-10% to-chart-2 to-100%'>
+          <div className='h-full w-full container mx-auto flex justify-between items-center border border-red-500'>
+            <div className="h-[90%] w-[47.5%] border border-yellow-500">
+              <Card>
+                <CardContent>
+                  Awkowakaw dowkodwa dksokowd awokaowkwa okwawko.
+                </CardContent>
+                <CardHeader>
+                  <CardTitle>
+                    Awakowak
+                  </CardTitle>
+                  <CardDescription>
+                    Aawokowa owkdowkda
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+            <div className="h-[90%] w-[47.5%] border border-yellow-500"></div>
           </div>
         </section>
         <section id='contact' className='h-[75vh] w-full'>
